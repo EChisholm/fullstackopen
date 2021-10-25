6802f88fc6879582b0972759cec88efc6717ecb3
EChisholm <evan.chisholm@gmail.com>
2021-10-24 18:55:07 -0700

    completed 3.4

--- part3/phonebook back-end/index.js
+++ part3/phonebook back-end/index.js
@@ -40,6 +40,12 @@ app.get('/api/persons/:id', (request,response) => {
     }
 })
 
+app.delete('/api/persons/:id', (request,response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end
+})
+
 app.get('/info', (request,response) => {
     const numPersons = persons.length
     response.send(`<p> Phonebook has info for ${numPersons} people<p>
