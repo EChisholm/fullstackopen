ff541038bc289ebcfbd86e81a26e78f3eaac6d5f
EChisholm <evan.chisholm@gmail.com>
2021-10-24 18:42:35 -0700

    completed 3.2

--- part3/phonebook back-end/index.js
+++ part3/phonebook back-end/index.js
@@ -29,6 +29,14 @@ app.get('/api/persons', (request,response) => {
     response.json(persons)
 })
 
+app.get('/info', (request,response) => {
+    const numPersons = persons.length
+    response.send(`<p> Phonebook has info for ${numPersons} people<p>
+                    <p> ${Date()} <p>
+    `)
+
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
